e2c8bbddf8a7806363fc89093233ed8140b8eb66
Denis T <dev@denis-it.com>
2016-11-21 09:57:56 +0300

    datetime prefix added to log messages

--- Log.pm
+++ Log.pm
@@ -7,6 +7,8 @@ use Log::Any ();
 use Log::Any::Adapter ('Stdout');
 use Log::Any::For::Std;
 
+use DateTimeFactory;
+
 my $sid = 0;
 sub incsid {
 	$sid++;
@@ -15,18 +17,23 @@ sub incsid {
 sub new {
 	my ($class, $category) = @_;
 	my $log = Log::Any->get_logger(category => $category // "general");
-	my $self = {log => $log};
+	my $self = {log => $log, dtf => DateTimeFactory->new};
 	bless $self, $class;
+}
+
+sub _prefix {
+	my ($self) = @_;
+	$self->{dtf}->now->datetime . " [$sid] ";
 }
 
 sub debugf {
 	my ($self, $message, @params) = @_;
-	$self->{log}->debugf("[$sid] " . $message, @params);
+	$self->{log}->debugf($self->_prefix . $message, @params);
 }
 
 sub infof {
 	my ($self, $message, @params) = @_;
-	$self->{log}->infof("[$sid] " . $message, @params);
+	$self->{log}->infof($self->_prefix . $message, @params);
 }
 
 1;
